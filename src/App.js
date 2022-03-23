4c89d48d0898ec306e49e0f7fabd0168a6a0b028
Your Name <you@example.com>
2022-03-23 15:18:32 -0600

    committing justify text project with tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [input, setInput] = useState(); 
+	const [length, setLength] = useState(); 
+	const [initialText, setInitialText] = useState();
+	const [justifiedText, setJustifiedText] = useState();
+	const [errorMessage, setErrorMessage] = useState();
+	
+	const justifyText = () => {
+	// Extra space in the justified length needs to be evenly distributed throughout 
+	// the given number of spaces in the initial text. 
+		// So count the number of spaces there are intially
+		if (input === undefined || input.trim() === "") {
+			setErrorMessage("Please enter a text to be justified.");
+		} else if (isNaN(length)) {
+			setErrorMessage("Please enter a valid number.");
+		} else if (parseInt(length) > 225) {
+			setErrorMessage("Please input a justified length of less than or equal to 225.")
+		} else {
+			let inputString = input + "";
+			let spaceCount = (inputString.split(" ").length - 1);
+			// Count how many letters of text there are. 
+			let lettersOfText = (inputString.replace(/\s/g, "").length - 1);
+			// Subtract the letters of text from justified length.
+			let spacesToDistribute = parseInt(length) - lettersOfText;
+			// Evenly distribute remaining spaces into the number of spaces found earlier. 
+			let charArray = inputString.trim().split('');
+			let addedSpaces = parseInt(spacesToDistribute / spaceCount);
+			
+			// Any remainder spaces can be evenly distributed starting from the beginnging of the sentence. 
+			let remainderSpaces = addedSpaces % spaceCount;
+			
+			let newJustifiedText = "";
+			for (let i = 0; i < charArray.length; i++) {
+				if (charArray[i] === " ") {
+					for (let spaces = addedSpaces; spaces >= 0; spaces--) {
+						newJustifiedText += "\u00A0";
+					}
+					if (remainderSpaces > 0) {
+						newJustifiedText += "\u00A0";
+						remainderSpaces--;
+					}
+				} else {
+					newJustifiedText += charArray[i];
+				}
+			}
+			setJustifiedText(newJustifiedText);
+			setInitialText(input);
+		}
+	}
+	
+	return (
+		<div className="App">
+			<header className="App-header">
+				Justify Your Text
+			</header>
+			<div className="form">
+				<div>
+					<label>The text to be justified:</label><input className="inputText" value={input || ""} onChange={val => setInput(val.target.value)} maxLength="100" data-testid="input-text" type="text" />
+				</div>
+				<div>
+					<label>The length the text should be justified to (max: 225):</label><input className="inputLength" value={length || 0} onChange={val => setLength(val.target.value)} data-testid="input-length" type="text" />
+					<button onClick={() => justifyText()} data-testid="justify-text-button">Justify Text</button>
+				</div>
+				<div>
+					<label value={initialText} onChange={val => setInitialText(val.target.value)} data-testid="initial-text-label">{initialText}</label>
+				</div>
+				<div>
+					<label value={justifiedText} onChange={val => setJustifiedText(val.target.value)} data-testid="justified-text-label">{justifiedText}</label>
+				</div>
+				<label className="red" value={errorMessage} onChange={val => setErrorMessage(val.target.value)} data-testid="error-message-label">{errorMessage}</label>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,109 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import {fireEvent} from '@testing-library/react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let container = null;
+
+beforeEach(() => {
+	container = document.createElement("div");
+	document.body.appendChild(container);
+});
+
+afterEach(() => {
+	unmountComponentAtNode(container);
+	container.remove();
+	container = null;
+});
+
+it("input text is non-empty", () => {
+	act(() => {
+		render(<App />, container);
+	});
+	
+	const input = document.querySelector("[data-testid=input-text]");
+	expect(input.textContent).toBe("");
+	
+	//const justifyText = jest.spyOn(App, 'justifyText');
+	const justifyTextButton = document.querySelector("[data-testid=justify-text-button]");
+	act(() => {
+		justifyTextButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+	});
+	
+	//expect(justifyText).toHaveBeenCalledTimes(1);
+	
+	const errorMessage = document.querySelector("[data-testid=error-message-label]");
+	expect(errorMessage.textContent).toBe("Please enter a text to be justified.");
+});
+
+it("length is a valid number less than or equal to 255", () => {
+	act(() => {
+		render(<App />, container);
+	});
+	
+	const length = document.querySelector("[data-testid=input-length]");
+	const input = document.querySelector("[data-testid=input-text]");
+	
+	fireEvent.change(input, {
+		target: {
+			value: "The quick brown fox jumps over the lazy dog."
+		}
+	});
+	
+	expect(length.value).toBe("0");
+	
+	fireEvent.change(length, {
+		target: {
+			value: 300
+		}
+	});
+	
+	const justifyTextButton = document.querySelector("[data-testid=justify-text-button]");
+	act(() => {
+		justifyTextButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+	});
+	
+	const errorMessage = document.querySelector("[data-testid=error-message-label]");
+	expect(errorMessage.textContent).toBe("Please input a justified length of less than or equal to 225.");
+	
+	fireEvent.change(length, {
+		target: {
+			value: "foo"
+		}
+	});
+	
+	act(() => {
+		justifyTextButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+	});
+	
+	expect(errorMessage.textContent).toBe("Please enter a valid number.");
+});
+
+it("justifyText method justifies text", () => {
+	act(() => {
+		render(<App />, container);
+	});
+	
+	const input = document.querySelector("[data-testid=input-text]");
+	const length = document.querySelector("[data-testid=input-length]");
+	
+	fireEvent.change(input, {
+		target: {
+			value: "The quick brown fox jumps over the lazy dog."
+		}
+	});
+	
+	fireEvent.change(length, {
+		target: {
+			value: 80
+		}
+	});
+	
+	const justifyTextButton = document.querySelector("[data-testid=justify-text-button]");
+	act(() => {
+		justifyTextButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+	});
+	
+	const outputLabel = document.querySelector("[data-testid=justified-text-label]");
+	expect(outputLabel.textContent).toBe("The       quick       brown       fox       jumps       over      the      lazy      dog.")
 });
